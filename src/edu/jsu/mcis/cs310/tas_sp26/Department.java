f187a7b73cb32ac783a2534febe33e0fc92fb8b6
cthrash16 <cthrash16@gmail.com>
2026-03-24 14:00:34 -0500

    Added Granular Object Serialization to Department, all tests pass

--- src/edu/jsu/mcis/cs310/tas_sp26/Department.java
+++ src/edu/jsu/mcis/cs310/tas_sp26/Department.java
@@ -3,8 +3,8 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package edu.jsu.mcis.cs310.tas_sp26;
-
-
+import com.github.cliftonlabs.json_simple.*;
+import java.io.*;
 import edu.jsu.mcis.cs310.tas_sp26.dao.DepartmentDAO;
 
 
@@ -14,7 +14,7 @@ import edu.jsu.mcis.cs310.tas_sp26.dao.DepartmentDAO;
  */
 
 
-public class Department {
+public class Department implements Jsonable {
 
     private int id;
     private String description;
@@ -45,4 +45,22 @@ public class Department {
             id, description, terminalid
         );
     }
+    @Override
+    public String toJson() {
+        return Jsoner.serialize(toJsonObject());
+    }
+
+    @Override
+    public void toJson(Writer writer) throws IOException {
+        toJsonObject().toJson(writer);
+    }
+
+    private JsonObject toJsonObject() {
+        JsonObject json = new JsonObject();
+        json.put("id", this.id);
+        json.put("description", this.description);
+        json.put("terminalid", this.terminalid);
+        return json;
+    }
 }
+
